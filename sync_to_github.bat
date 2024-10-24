b7df8eb654dadeadd37d24dd388a22831bad4512
harshakalluri1403 <142888968+harshakalluri1403@users.noreply.github.com>
2024-10-24 06:27:09 +0530

    Auto-sync on 24-10-2024  6:27:09.55

--- sync_to_github.bat
+++ sync_to_github.bat
@@ -2,4 +2,4 @@
 cd C:\Users\harsh\Desktop\Leetcode
 git add .
 git commit -m "Auto-sync on %date% %time%"
-git push origin main --force
+git push https://<harshakalluri14>:<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>@github.com/harshakalluri1403/Leet-Code.git main --force
