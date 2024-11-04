841433c8de0cd1d8b55a5414afa15dcc55151440
Pavan Sri Harsha Kalluri <harshakalluri14@gmail.com>
2024-11-04 21:48:00 +0530

    Delete Python directory

--- Python/Leetcode/66. Plus One.py
+++
@@ -1,5 +0,0 @@
-class Solution(object):
-    def plusOne(self, digits):
-        result = int(''.join(map(str, digits)))
-        result += 1
-        return [int(x) for x in str(result)]
--- Python/Leetcode/88. Merge Sorted Array.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-
-        nums1[m:] = nums2[:n]
-        nums1.sort()
-        
-        nums1[:] = [x for x in nums1 if x != 0]
-        
-        print(nums1)
