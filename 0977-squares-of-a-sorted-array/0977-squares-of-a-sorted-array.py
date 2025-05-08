b45fd91b2320c140cf3aca71c8ff584ea4fadf57
Pavan Sri Harsha Kalluri <harshakalluri14@gmail.com>
2025-05-08 18:27:23 +0530

    Time: 11 ms (69.04%), Space: 14.3 MB (38.88%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,4 +1,6 @@
 class Solution(object):
+    ## TIME COMPLEXITY FOR BOTH IS O(nlogn) 
+    ## 2nd method is more memory efficient thant 1st one 
     #def sortedSquares(self, nums):
     #    return sorted(list(map(lambda i: i ** 2, nums)))
     def sortedSquares(self, nums):
