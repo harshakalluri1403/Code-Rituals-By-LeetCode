e032d62e535c0775ec58c84946e302b15ce589a0
Pavan Sri Harsha Kalluri <harshakalluri14@gmail.com>
2025-05-11 17:42:34 +0530

    Time: 27 ms (26.46%), Space: 13.4 MB (40.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,3 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if sorted(s)==sorted(t):
-            return True
-        else:
-            return False
+        return sorted(s)==sorted(t)
