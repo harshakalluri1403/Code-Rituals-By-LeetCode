40fe8a245e07694db6d0a4c16b6a042aed311e7a
Pavan Sri Harsha Kalluri <harshakalluri14@gmail.com>
2025-12-30 00:09:47 +0530

    Time: 0 ms (100%), Space: 17.2 MB (96.25%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,10 +1,4 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-
-        nums1[m:] = nums2[:n]
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums1[m:]=nums2[:n]
         nums1.sort()
-        
-        # Now remove any remaining zeros from the sorted array (if there are any)
-        nums1[:] = [x for x in nums1 if x != 0]
-        
-        print(nums1)
