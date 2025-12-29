d3a4944401687b6fe049412710aeee7419bd0eac
Pavan Sri Harsha Kalluri <harshakalluri14@gmail.com>
2025-12-29 23:46:33 +0530

    Time: 2483 ms (6.98%), Space: 17.7 MB (97.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        num_to_index = {} 
-        
-        for index, num in enumerate(nums):
-            complement = target - num  # Calculate the required complement
-            
-            if complement in num_to_index:  # Check if the complement is in the dictionary
-                return [num_to_index[complement], index]  # Return the indices of the complement and the current number
-            
-            num_to_index[num] = index 
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sum=0
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if target-nums[i]==nums[j]:
+                    return i,j
+
+
