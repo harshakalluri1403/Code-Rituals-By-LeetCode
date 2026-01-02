8f51530a84db835ae3d8ec519d1cc47e5347253c
Pavan Sri Harsha Kalluri <harshakalluri14@gmail.com>
2026-01-02 21:52:03 +0530

    Time: 8 ms (52.02%), Space: 17.5 MB (95.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        filtered = ''.join(char.lower() for char in s if char.isalnum())
-        return filtered == filtered[::-1]
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new=""
+        for char in s:
+            if char.isalnum():
+                new=new+char
+        str=new.lower()
+        left=0
+        right=len(str)-1
+        while left<=right:
+            if str[left]==str[right]:
+                left+=1
+                right-=1
+            else:
+                return False
+        return True
+
